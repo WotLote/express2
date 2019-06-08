25c24cbf2753fe2fa812c4fdafb6413f1ccfae2b
WotLote <jerab36rum@gmail.com>
2019-06-08 04:14:11 +0400

    1

--- api/date.js
+++ api/date.js
@@ -6,10 +6,8 @@ const app = express();
 
 app.use(helmet());
 
-app.get('*', (req, res) => {
-  res.set('Content-Type', 'text/html');
-  const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-  res.status(200).send(currentTime);
+app.get('/', (req, res) => {
+  res.status(200);
 });
 
 module.exports = app;
---
+++ www/precache-manifest.f95e83c5743c4cc64d7be088e0d6f2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17630eef045048dfab896616e987eb44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf288953e6df162bd430",
+    "url": "/static/css/2.872807ca.chunk.css"
+  },
+  {
+    "revision": "5ea44346750044b93469",
+    "url": "/static/css/main.bfa4502e.chunk.css"
+  },
+  {
+    "revision": "bf288953e6df162bd430",
+    "url": "/static/js/2.31a34ebc.chunk.js"
+  },
+  {
+    "revision": "5ea44346750044b93469",
+    "url": "/static/js/main.79f741ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
